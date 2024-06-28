3823280b5a4bb6b17914d4de7b5791ec5726b407
otakota <otakota268@gmail.com>
2024-06-29 03:35:14 +0900

    konnitiha

--- server/api/novel/controller.ts
+++ server/api/novel/controller.ts
@@ -2,6 +2,6 @@ import { novelUseCase } from 'domain/novel/usecase/novelUseCase';
 import { defineController } from './$relay';
 
 export default defineController(() => ({
-  get: () => ({ status: 200, body: 'Hello' }),
+  get: () => ({ status: 200, body: 'こんにちは' }),
   post: async ({ body }) => ({ status: 200, body: await novelUseCase.scrape(body.aozoraUrl) }),
 }));
