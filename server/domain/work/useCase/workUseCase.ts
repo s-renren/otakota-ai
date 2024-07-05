27eeaa3979bcd662ecb6fbaca6e2e770da234643
otakota <otakota268@gmail.com>
2024-07-06 06:00:36 +0900

    put text

--- server/domain/work/useCase/workUseCase.ts
+++ server/domain/work/useCase/workUseCase.ts
@@ -1,5 +1,6 @@
 import { LoadingWorkEntity } from 'api/@types/work';
 import { transaction } from 'service/prismaClient';
+import { s3 } from 'service/s3Client';
 import { workEvent } from '../event/workEvent';
 import { workMethod } from '../model/workMethod';
 import { novelQuery } from '../repository/novelQuery';
@@ -12,6 +13,7 @@ export const workUseCase = {
       const loadingWork = workMethod.create({ novelUrl, title, author });
 
       await workCommand.save(tx, loadingWork);
+      await s3.putText(`works/${loadingWork.id}/content.txt`, html);
 
       workEvent.workCreated({ loadingWork, html });
 
@@ -22,6 +24,7 @@ export const workUseCase = {
       const completeWork = workMethod.complete(loadingwork);
 
       await workCommand.save(tx, completeWork);
+      await s3.putImage(`works/${loadingwork.id}/image.png`, image);
     }),
 
   failure: (loadingWork: LoadingWorkEntity, errorMsg: string): Promise<void> =>
