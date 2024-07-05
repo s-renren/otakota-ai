4d4de3189a7381ee5ca8a93bf2142691ddb8acc2
otakota <otakota268@gmail.com>
2024-07-05 22:38:32 +0900

    urlもってこれるようになった22:38

--- server/domain/work/useCase/workUseCase.ts
+++ server/domain/work/useCase/workUseCase.ts
@@ -1,11 +1,13 @@
 import { LoadingWorkEntity } from 'api/@types/work';
 import { transaction } from 'service/prismaClient';
 import { workMEthod } from '../model/workMethod';
+import { novelQuery } from '../repository/novelQuery';
 import { workCommand } from '../repository/workCommand';
 
 export const workUseCase = {
-  create: (novelUrl: string, title: string, author: string): Promise<LoadingWorkEntity> =>
+  create: (novelUrl: string): Promise<LoadingWorkEntity> =>
     transaction('RepeatableRead', async (tx) => {
+      const { title, author } = await novelQuery.scrape(novelUrl);
       const loadingWork = workMEthod.create({ novelUrl, title, author });
 
       await workCommand.save(tx, loadingWork);
